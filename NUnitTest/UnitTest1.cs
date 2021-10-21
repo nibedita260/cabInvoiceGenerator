576845f5fadfdd507ba0afd981bf471d520ce973
nibedita260 <deepi.nibedita260.com>
2021-10-21 20:20:14 +0530

    [Nibedita] Add: CalculateFareForMultipleRides For CabInvoiceGenerator Features Implemented

--- NUnitTest/UnitTest1.cs
+++ NUnitTest/UnitTest1.cs
@@ -32,5 +32,30 @@ namespace NUnitTest
                 Assert.AreEqual("Cost Could not LessThan Minimum Cost", e.Message);
             }
         }
+        //Test-2.1 ->Given Avg cost should matches the multiple cab rides cost
+        [Test]
+        public void Given_Avgcost_ShouldMatches_ToActualAvgCost()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            Ride[] rides = { new Ride(3.0, 5.0), new Ride(2.0, 5.0) };
+            double actual = invoiceGenerator.CalMultipleRidesFare(rides);
+            Assert.AreEqual(30, actual);
+        }
+        //Test-1.2 ->When ActualAvg Cost Less than MinimumCost Throw Exception
+        [Test]
+        public void When_ActualAvgCost_LessThanMinimumCost_ThrowException()
+        {
+            try
+            {
+                InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+                Ride[] rides = { new Ride(3.0, 5.0), new Ride(2.0, 5.0) };
+                double actual = invoiceGenerator.CalMultipleRidesFare(rides);
+                Assert.AreEqual(30, actual);
+            }
+            catch (CabInvoiceGeneratorCustomException e)
+            {
+                Assert.AreEqual("Cost Could not LessThan Minimum Cost", e.Message);
+            }
+        }
     }
 }
